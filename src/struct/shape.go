53746eb281c1b88d8927b98f0a7be9b82f8e1a05
yuyahy <xvto.ya@gmail.com>
2025-02-15 08:33:47 +0000

    feat: "構造体、メソッド、インターフェース完了"

--- src/struct/shape.go
+++ src/struct/shape.go
@@ -7,12 +7,21 @@ type Rectangle struct {
 	Height float64
 }
 
-type Shape interface {
-	Area() float64
-}
-
 func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
+}
+
+type Shape interface {
+	Area() float64
 }
 
 type Circle struct {
