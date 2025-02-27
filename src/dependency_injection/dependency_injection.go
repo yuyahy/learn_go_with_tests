4d68530f7b813a11b800109d512555bf8d849c61
yuyahy <xvto.ya@gmail.com>
2025-02-27 18:32:19 +0900

    feat: "依存性注入"完了 (#9)
    * feat: "依存性注入"完了
    
    * fix: errcheckを追加

---
+++ src/dependency_injection/dependency_injection.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	//	fmt.Println("aaaaaa")
+	error := http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+	fmt.Printf("unexpected error : %q", error)
+}
