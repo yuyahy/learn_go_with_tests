442e4764cd4e9c206c2ccf9235578f232c03ddd7
yuyahy <xvto.ya@gmail.com>
2025-03-05 12:45:49 +0000

    feat: "select"完了

---
+++ src/racer/racer.go
@@ -0,0 +1,42 @@
+package racer
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	// 複数のチャネルのうち、どれかの準備が完了するまでブロック
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	// channelのゼロ値はnilなので、var ch chan struct{}で初期化すると、
+	// ch <- が永遠に届かないことになる
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
