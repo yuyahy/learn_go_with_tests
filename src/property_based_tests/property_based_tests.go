bd9d2a25b8a2fc518e627077dd6dbf3bcde062c3
yuyahy <xvto.ya@gmail.com>
2025-03-24 12:59:34 +0000

    feat: {"5 gets converted to V", 5, "V"}の手前まで

---
+++ src/property_based_tests/property_based_tests.go
@@ -0,0 +1,17 @@
+package propertybasedtests
+
+import "strings"
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
+
+		result.WriteString("I")
+	}
+	return result.String()
+}
