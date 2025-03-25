27ea7289764d149a10ff03cc02b3195ef43b4b36
yuyahy <xvto.ya@gmail.com>
2025-03-25 12:36:59 +0000

    feat: Here are some test cases, try and make them pass. まで

--- src/property_based_tests/property_based_tests.go
+++ src/property_based_tests/property_based_tests.go
@@ -2,16 +2,27 @@ package propertybasedtests
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for i := arabic; i > 0; i-- {
-		if i == 4 {
-			result.WriteString("IV")
-			break
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
-
-		result.WriteString("I")
 	}
 	return result.String()
 }
--- src/property_based_tests/property_based_tests_test.go
+++ src/property_based_tests/property_based_tests_test.go
@@ -12,6 +12,17 @@ func TestRomanNumerals(t *testing.T) {
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
 		{"4 gets converted to IV(can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"18 gets converted to XVIII", 18, "XVIII"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
+		{"40 gets converted to XL", 40, "XL"},
+		{"47 gets converted to XLVII", 47, "XLVII"},
+		{"49 gets converted to XLIX", 49, "XLIX"},
+		{"50 gets converted to L", 50, "L"},
 	}
 
 	for _, test := range cases {
