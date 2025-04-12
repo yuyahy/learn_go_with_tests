89e2e9fbdfde46690a6d56604ed0bb1294793854
yuyahy <xvto.ya@gmail.com>
2025-04-12 07:16:25 +0000

    feat: secondsInRadiansを固定でmath.Piを返すところまで

--- src/clockface/clockface.go
+++ src/clockface/clockface.go
@@ -1,12 +1,21 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
+// Point is a 2D Cartesian coordinate
 type Point struct {
 	X float64
 	Y float64
 }
 
+// 時刻から秒針の先端の座標を返す
 func SecondHand(t time.Time) Point {
 	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi
 }
--- src/clockface/clockface_test.go
+++ src/clockface/clockface_test.go
@@ -1,10 +1,12 @@
 package clockface
 
 import (
+	"math"
 	"testing"
 	"time"
 )
 
+// 0時0分0秒の時の秒針の位置をテスト
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
@@ -13,5 +15,26 @@ func TestSecondHandAtMidnight(t *testing.T) {
 
 	if got != want {
 		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+
+// 0時0分30秒の時の秒針の位置をテスト
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+	expected := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+
+	if got != expected {
+		t.Errorf("Got %v, expected %v", got, expected)
+	}
+}
+
+func TestSecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
+
+	if want != got {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
 	}
 }
