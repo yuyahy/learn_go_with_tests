2ac3e92402e281309a08b6b61001af383ba36b5a
yuyahy <xvto.ya@gmail.com>
2025-04-24 01:05:55 +0000

    fix: テストが通るように修正

--- src/clockface/clockface_test.go
+++ src/clockface/clockface_test.go
@@ -109,13 +109,13 @@ func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
 	b := bytes.Buffer{}
-	clockface.SVGWriter(&b, tm)
+	SVGWriter(&b, tm)
 
 	svg := Svg{}
 	xml.Unmarshal(b.Bytes(), &svg)
 
-	x2 := "150"
-	y2 := "60"
+	x2 := "150.000"
+	y2 := "60.000"
 
 	for _, line := range svg.Line {
 		if line.X2 == x2 && line.Y2 == y2 {
